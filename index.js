b38c449df0d3064d6b0bbd0fee50604b0245aa61
Robinder Dhillon <rb802397@dal.ca>
2021-07-21 21:34:10 -0300

    changed port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 2000;
+const port = process.env.PORT || 3000
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
