3e3d042a2d2175bc9919c2b4f222c90be08d0c5d
robin1995dhillon <58319527+robin1995dhillon@users.noreply.github.com>
2021-07-29 01:33:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const server = http.createServer(app);
 var mysql = require('mysql');
 
 var bodyParser = require('body-parser');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const uniqid = require('uniqid');
